0563737a691b1e94a3de35b19a3c0e9a7554c20a
xu_yanfeng <xu_yanfeng@126.com>
2024-07-05 16:12:55 +0800

    bugfix

--- cc-plugin.config.ts
+++ cc-plugin.config.ts
@@ -1,46 +1,46 @@
 // @ts-ignore
-import { CocosPluginManifest, CocosPluginOptions, Panel, PluginType } from 'cc-plugin/src/declare';
+import { CocosPluginManifest, CocosPluginOptions, Panel, PluginType } from "cc-plugin/src/declare";
 
-const pkgName = 'excel-killer';
+const pkgName = "excel-killer";
 
 const manifest: CocosPluginManifest = {
-    name: pkgName,
-    version: '1.0.0',
-    description: 'excel-killer',
-    author: 'cc-plugin',
-    main: './src/main.ts',
-    panels: [
-        {
-            name: 'main',
-            type: Panel.Type.DockAble,
-            main: './src/panel/index.ts',
-            title: 'excel-killer',
-            width: 500,
-            height: 400,
-            minWidth: 50,
-            minHeight: 400,
-        },
-    ],
-    menus: [
-        {
-            path: `i18n.title`,
-            message: {
-                name: 'showPanel',
-            },
-        },
-    ],
-    i18n_en: './src/i18n/en.ts',
-    i18n_zh: './src/i18n/zh.ts',
+  name: pkgName,
+  version: "0.2",
+  description: "excel-killer",
+  author: "cc-plugin",
+  main: "./src/main.ts",
+  panels: [
+    {
+      name: "main",
+      type: Panel.Type.DockAble,
+      main: "./src/panel/index.ts",
+      title: "excel-killer",
+      width: 500,
+      height: 400,
+      minWidth: 50,
+      minHeight: 400,
+    },
+  ],
+  menus: [
+    {
+      path: `i18n.title`,
+      message: {
+        name: "showPanel",
+      },
+    },
+  ],
+  i18n_en: "./src/i18n/en.ts",
+  i18n_zh: "./src/i18n/zh.ts",
 };
 // 这里的options变量名暂时不支持修改，发布时会进行必要的修改
 const options: CocosPluginOptions = {
-    server: {
-        enabled: true,
-        port: 2022,
-    },
-    watchBuild: true,
-    outputProject: {
-        web: './web',
-    },
+  server: {
+    enabled: true,
+    port: 2022,
+  },
+  watchBuild: true,
+  outputProject: {
+    web: "./web",
+  },
 };
 export default { manifest, options };
